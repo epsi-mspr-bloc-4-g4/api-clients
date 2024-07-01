6a5305facc64bdad366d8aea987487a256672b68
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-07-02 00:08:00 +0200

    fix pipeline (#34)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import './../instrument.js';
 import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
@@ -12,16 +12,26 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+Sentry.init({
+  dsn: "https://23a486814f480b9ff425587db31ff137@o4507378463080448.ingest.de.sentry.io/4507486532272208",
+  integrations: [
+      nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
 const app = express();
 
 app.use(cookieParser());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
-app.use(errorHandler);
 Sentry.setupExpressErrorHandler(app);
 
 app.get("/debug-sentry", function mainHandler(req, res) {
